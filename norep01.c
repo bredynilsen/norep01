52b36eefc6fcec2bd77ff6516b6c3218e65709fb
Bredy Nilsen Orrico Valentim <bredynilsen@gmail.com>
2017-04-14 19:58:12 -0300

    norep usando libredy.h

--- norep01.c
+++ norep01.c
@@ -31,24 +31,3 @@ return 0;
 getchar();
 system("pause");
 }
-
-/*Função para preenchimento do vetor e comparação de números iguais*/
-/*void norep(int x, int *iVetor) 
-{
-    int k, j, iAux, iFlag = 0;
-
-    for (k = 0; k < x; k++) 
-    {
-        iAux = 1+ rand()%6;
-        for (j = 0; j < k; j++) 
-        {
-            if (iAux == iVetor[j])
-                iFlag = 1;
-        }
-        if (iFlag != 1)
-            iVetor[k] = iAux;
-        else
-            k--;
-        iFlag = 0;
-    }
-}*/
